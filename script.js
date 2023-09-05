74bc9ac2cb7af1050b9efb50723039a597ab2053
joaovitorangelo <joaovitorangelo05@gmail.com>
2023-09-05 00:28:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//perguntaUM
+
+function correct1(resposta1) {
+    var respostaDiv = document.getElementById("resposta1");
+    respostaDiv.innerHTML = resposta1;
+}
+
+function wrong1(resposta1) {
+    var respostaDiv = document.getElementById("resposta1");
+    respostaDiv.innerHTML = resposta1;
+}
+
+//perguntaDOIS
+
+function correct2(resposta2) {
+    var respostaDiv = document.getElementById("resposta2");
+    respostaDiv.innerHTML = resposta2;
+}
+
+function wrong2(resposta2) {
+    var respostaDiv = document.getElementById("resposta2");
+    respostaDiv.innerHTML = resposta2;
+}
+
+//perguntaTRES
+
+function correct3(resposta3) {
+    var respostaDiv = document.getElementById("resposta3");
+    respostaDiv.innerHTML = resposta3;
+}
+
+function wrong3(resposta3) {
+    var respostaDiv = document.getElementById("resposta3");
+    respostaDiv.innerHTML = resposta3;
+}
